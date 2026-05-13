64338e45e43656a3dd09702fd534654f8ec76838
kelvinokumu <oumakelvinokumu@gmail.com>
2026-05-13 07:24:35 +0300

    added sum recursion

--- 07_recursion/07_factorial_recursion.py
+++ 07_recursion/07_factorial_recursion.py
@@ -7,14 +7,17 @@ def factorial(n):
 res = factorial(4)
 print(res)
 
+# factorial(5)
+# = 5 * factorial(4)
+# = 5 * (4 * factorial(3))
+# = 5 * (4 * (3 * factorial(2)))
+# = 5 * (4 * (3 * (2 * factorial(1))))
+# = 5 * (4 * (3 * (2 * (1 * factorial(0)))))
+# = 5 * (4 * (3 * (2 * (1 * 1))))
 
-factorial(4)
-# = 4 * factorial(3)
-#
-# = 4 * (3 * factorial(2))
-#
-# = 4 * (3 * (2 * factorial(1)))
-#
-# = 4 * (3 * (2 * (1 * factorial(0))))
-#
-# = 4 * (3 * (2 * (1 * 1)))
+# factorial(0) = 1
+# factorial(1) = 1 * 1 = 1
+# factorial(2) = 2 * 1 = 2
+# factorial(3) = 3 * 2 = 6
+# factorial(4) = 4 * 6 = 24
+# factorial(5) = 5 * 24 = 120
