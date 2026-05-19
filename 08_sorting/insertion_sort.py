f22b72367172ef94ec04b870d674290a7a9fbbb9
kelvinokumu <oumakelvinokumu@gmail.com>
2026-05-19 15:16:17 +0300

    insertion sort

--- 08_sorting/insertion_sort.py
+++ 08_sorting/insertion_sort.py
@@ -1,17 +1,3 @@
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-#         j = i - 1
-#         while j >= 0 and key < arr[j]:
-#             arr[j + 1] = arr[j]
-#             j -= 1
-#             j = j - 1
-#         arr[j + 1] = key
-#     return arr
-#
-#
-# print(insertion_sort([8, 4, 1, 5, 9]))
-
 import random
 
 
